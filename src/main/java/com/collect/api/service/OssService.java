63f62bfdff2c385ec59fbecfafb120f872201e31
zhangxingyun <Administrator@PC-20181103LBKC>
2019-01-14 15:24:43 +0800

    (add) service

--- src/main/java/com/collect/api/service/OssService.java
+++ src/main/java/com/collect/api/service/OssService.java
@@ -1,13 +1,17 @@
 package com.collect.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.collect.api.bean.Oss;
 import com.collect.api.dao.OssDao;
+import com.collect.api.oss.config.CloudStorageConfig;
 import com.collect.common.base.BaseService;
+import com.collect.common.utils.Constant;
 import com.collect.common.utils.IdGen;
 
+@Service
 public class OssService extends BaseService<OssDao, Oss>{
 	
 	@Autowired
@@ -16,10 +20,13 @@ public class OssService extends BaseService<OssDao, Oss>{
 	@Transactional(readOnly = false)
 	public boolean insert(Oss entity) {
 		entity.setId(IdGen.getObjectId());
-		return true;
+		if (getCloudStorageConfig().getType() == Constant.CloudService.LOCAL.getValue()) {
+			entity.setUrl(entity.getUrl() + entity.getId());
+		}
+		return super.insert(entity);
 	}
 	
-//	private CloudStorageConfig getCloudStorageConfig() {
-//		return configService.getConfigObject("oss.config", CloudStorageConfig.class);
-//	}
+	private CloudStorageConfig getCloudStorageConfig() {
+		return configService.getConfigObject("oss.config", CloudStorageConfig.class);
+	}
 }
