53feeedb0cbd38e3b9320c765ce7b9bb59528fa7
zhangxingyun <Administrator@PC-20181103LBKC>
2019-01-18 17:40:51 +0800

    (add) 增加门户页面

--- src/main/java/com/collect/api/web/TemplateController.java
+++ src/main/java/com/collect/api/web/TemplateController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TemplateController {
 	
-	@RequestMapping(value = {"/", "/index"})
-	public String index() {
-		return "index";
+	@RequestMapping(value = {"/"})
+	public String home() {
+		return "home";
 	}
 	
 	@RequestMapping(value = {"/404"})
 	public String error() {
 		return "404";
+	}
+	
+	@RequestMapping(value = {"/index"})
+	public String index() {
+		return "index";
 	}
 }
