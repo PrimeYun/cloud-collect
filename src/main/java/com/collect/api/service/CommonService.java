5a683ba8c5575d5f3faf966d815c914fa015f12a
zhangxingyun <10617@DESKTOP-56ACGUV.lan>
2019-06-16 13:23:27 +0800

    (edit) 取消部分数据

--- src/main/java/com/collect/api/service/CommonService.java
+++ src/main/java/com/collect/api/service/CommonService.java
@@ -4,24 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.collect.api.bean.CollectSort;
-import com.collect.api.bean.IpAddressInfo;
-import com.collect.api.bean.WeatherResponse;
-import com.collect.common.utils.HttpContextUtils;
-import com.collect.common.utils.IPUtils;
-import com.github.pagehelper.util.StringUtil;
 import com.google.common.collect.Maps;
-
-import cn.hutool.http.HttpUtil;
-
-import com.alibaba.fastjson.JSONObject;
 
 @Service
 public class CommonService {
@@ -32,14 +21,14 @@ public class CommonService {
 	@Autowired
 	private StringRedisTemplate stringTemplate;
 	
-	@Autowired
-	private SysConfigService configService;
+//	@Autowired
+//	private SysConfigService configService;
 	
 	public Map<String, Object> get() {
 		Map<String, Object> result = Maps.newHashMap();
 		result.put("num", getViewNum());
 		result.put("sort", getSort());
-		result.put("weather", getWeather());
+		// result.put("weather", getWeather());
 		return result;
 	}
 	
@@ -56,33 +45,33 @@ public class CommonService {
 		return sortService.selectList(result);
 	}
 	
-	private WeatherResponse getWeather() {
-		WeatherResponse item = null;
-		HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
-		String ipAddress = IPUtils.getIpAddr(request);
-		// 本地无法获取正确的天气信息，默认为镇江
-		if ("0:0:0:0:0:0:0:1".equals(ipAddress))
-			ipAddress = "222.186.125.185";
-		String value = stringTemplate.opsForValue().get("weather:" + ipAddress);
-		
-		if (StringUtil.isEmpty(value)) {
-			String location = this.getLocation(ipAddress);
-			value = HttpUtil.get(configService.getValue("weather.url") + location);
-			stringTemplate.opsForValue().set("weather:" + ipAddress, value);
-		}
-		
-		item = JSONObject.parseObject(value, WeatherResponse.class);
-		item.setData(item.getData().subList(0, 1));
-		return item;
-	}
-	
-	private String getLocation(String address) {
-        String location = HttpUtil.get(configService.getValue("ip.address") + address);
-        IpAddressInfo item = null;
-        if (StringUtil.isNotEmpty(location)) {
-        	item = JSONObject.parseObject(location, IpAddressInfo.class);
-        }
-        return item == null ? null : item.getCity().substring(0, item.getCity().lastIndexOf("市"));
-	}
+//	private WeatherResponse getWeather() {
+//		WeatherResponse item = null;
+//		HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
+//		String ipAddress = IPUtils.getIpAddr(request);
+//		// 本地无法获取正确的天气信息，默认为镇江
+//		if ("0:0:0:0:0:0:0:1".equals(ipAddress))
+//			ipAddress = "222.186.125.185";
+//		String value = stringTemplate.opsForValue().get("weather:" + ipAddress);
+//		
+//		if (StringUtil.isEmpty(value)) {
+//			String location = this.getLocation(ipAddress);
+//			value = HttpUtil.get(configService.getValue("weather.url") + location);
+//			stringTemplate.opsForValue().set("weather:" + ipAddress, value);
+//		}
+//		
+//		item = JSONObject.parseObject(value, WeatherResponse.class);
+//		item.setData(item.getData().subList(0, 1));
+//		return item;
+//	}
+//	
+//	private String getLocation(String address) {
+//        String location = HttpUtil.get(configService.getValue("ip.address") + address);
+//        IpAddressInfo item = null;
+//        if (StringUtil.isNotEmpty(location)) {
+//        	item = JSONObject.parseObject(location, IpAddressInfo.class);
+//        }
+//        return item == null ? null : item.getCity().substring(0, item.getCity().lastIndexOf("市"));
+//	}
 	
 }
